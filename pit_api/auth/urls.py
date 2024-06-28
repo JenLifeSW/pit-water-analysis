ace0f089103ae4c17e86385c01b68ede54b171a1
준하 노 <tnptt1122@gmail.com>
2024-06-28 14:01:10 +0900

    feat: 사용하지 않는 API 호출 제한

--- pit_api/auth/urls.py
+++ pit_api/auth/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("/registration", RegistrationAPIView.as_view()),
     path("/login", LoginAPIView.as_view()),
     path("/refresh", RefreshTokenAPIView.as_view()),
-    path("/send-verification-email", SendVerificationEmail.as_view()),
-    path("/verify-email", VerifyEmail.as_view()),
+    # path("/send-verification-email", SendVerificationEmail.as_view()),
+    # path("/verify-email", VerifyEmail.as_view()),
 ]
