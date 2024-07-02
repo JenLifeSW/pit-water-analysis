075f68b1ddb48477d1882c119be1dd8bed016a94
준하 노 <tnptt1122@gmail.com>
2024-07-03 00:25:59 +0900

    feat: 측정 타겟 초기값 추가

--- pit_api/apps.py
+++ pit_api/apps.py
@@ -5,6 +5,8 @@ from django.db.models.signals import post_migrate
 def create_initial_roles(sender, **kwargs):
     from pit_api.auth.models import Role
     from pit_api.fish_species.models import FishSpecies
+    from pit_api.measurements.models import MeasurementTarget
+
     if not Role.objects.exists():
         Role.objects.create(name="user", description="사용자", id=0)
         Role.objects.create(name="manager", description="매니저", id=10)
@@ -13,6 +15,9 @@ def create_initial_roles(sender, **kwargs):
 
     if not FishSpecies.objects.exists():
         FishSpecies.objects.create(name="기타")
+
+    if not MeasurementTarget.objects.exists():
+        MeasurementTarget.objects.create(name="질산성 질소", display_unit="ppm", display_multiplier="0.001")
 
 
 class PitApiConfig(AppConfig):
