51ccbbfa8aa63eb2c9b4638cb4e9ae0a590c0159
준하 노 <tnptt1122@gmail.com>
2024-07-03 09:43:48 +0900

    fix: 타겟 물질 농도 배율 바르지 않던 현상 수정

--- pit_api/measurements/serializers.py
+++ pit_api/measurements/serializers.py
@@ -39,8 +39,8 @@ class MeasurementDataSerializer(BaseSerializer):
     def _get_grade_standard(self, obj):
         grade_standard = GradeStandard.objects.filter(
             target=obj.target,
-            min_value__lte=obj.value,
-            max_value__gte=obj.value
+            min_value__lte=self.get_value(obj),
+            max_value__gte=self.get_value(obj)
         ).first()
 
         return grade_standard
@@ -62,3 +62,10 @@ class MeasurementHistorySerializer(BaseSerializer):
         fields = ["measuredAt", "value"]
 
     measuredAt = serializers.DateTimeField(source="measured_at", format="%Y-%m-%dT%H:%M:%S")
+    value = serializers.SerializerMethodField()
+
+    def get_value(self, obj):
+        target = obj.target
+        value = obj.value
+
+        return round(value * target.display_multiplier, 4)
