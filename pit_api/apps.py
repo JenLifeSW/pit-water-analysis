fdbbdf8ef89cd5e3d3576a6a1316a5dde1b12e13
준하 노 <tnptt1122@gmail.com>
2024-07-12 16:05:31 +0900

    feat: 초기 데이터 추가

--- pit_api/apps.py
+++ pit_api/apps.py
@@ -6,6 +6,7 @@ def create_initial_datas(sender, **kwargs):
     from pit_api.auth.models import Role
     from pit_api.fish_species.models import FishSpecies
     from pit_api.measurements.models import MeasurementTarget
+    from pit_api.grades.models import Grade
 
     if not Role.objects.exists():
         Role.objects.create(name="user", description="사용자", id=0)
@@ -20,6 +21,12 @@ def create_initial_datas(sender, **kwargs):
 
     if not MeasurementTarget.objects.exists():
         MeasurementTarget.objects.create(name="질산성 질소", display_unit="ppm", display_multiplier="0.001")
+        MeasurementTarget.objects.create(name="온도", display_unit="˚C", display_multiplier=1)
+
+    if not Grade.objects.exists():
+        Grade.objects.create(name="안전", color="#00FF00")
+        Grade.objects.create(name="경고", color="#FFA500")
+        Grade.objects.create(name="위험", color="#FF0000")
 
 
 class PitApiConfig(AppConfig):
