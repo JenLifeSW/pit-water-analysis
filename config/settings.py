e072b3226d50e44f285868b6f08f2282ffea3bde
준하 노 <tnptt1122@gmail.com>
2024-07-16 15:49:57 +0900

    style: 어세스 토큰 유효기간 30일 임시 연장

--- config/settings.py
+++ config/settings.py
@@ -110,7 +110,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = "pit_api.User"
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
